20b7d56aaa773cb16192d5e9f1ffe23ccf98a245
Alley1818 <llzeynolla@vk.com>
2026-05-03 17:58:38 +0500

    fix: type for cookiesToSet in middleware

--- middleware.ts
+++ middleware.ts
@@ -9,12 +9,15 @@ export async function middleware(request: NextRequest) {
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        getAll() { return request.cookies.getAll() },
-        setAll(cookiesToSet) {
+        getAll() {
+          return request.cookies.getAll()
+        },
+        // Добавляем строгую типизацию здесь:
+        setAll(cookiesToSet: { name: string; value: string; options: CookieOptions }[]) {
           cookiesToSet.forEach(({ name, value }) => request.cookies.set(name, value))
           supabaseResponse = NextResponse.next({ request })
           cookiesToSet.forEach(({ name, value, options }) =>
-            supabaseResponse.cookies.set(name, value, options)
+              supabaseResponse.cookies.set(name, value, options)
           )
         },
       },
